186fef2e5e6b184efb673a9cff8fd87bf249095d
deartrudence <trudy.macnabb@gmail.com>
2015-07-15 16:34:34 -0400

    redo :(

--- header.php
+++ header.php
@@ -14,7 +14,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/styles/style.css">
 
 <?php wp_head(); ?>
 </head>
@@ -23,27 +22,13 @@
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'triumph' ); ?></a>
 
+	<header id="masthead" class="site-header" role="banner">
+		<div class="site-branding">
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+		</div><!-- .site-branding -->
 
-	<header id="masthead" class="home__header" role="banner">
-		<div class="heroImg" style="background-image: url('<?php echo get_site_url() ?>/wp-content/uploads/2015/07/rev-1.jpg'); ">
-			<div class="site-branding">
-				<h2 class="header__branding"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h2>
-			</div><!-- .site-branding -->
-			<div class="container home__container">
-			<h2 class="header__title">
-				PROVIDING YOU WITH CONFIDENCE THAT
-			</h2>
-			<h2 class="header__title">
-				YOUR FAMILY AND WELL BEING ARE
-			</h2>
-			<h2 class="header__title">
-				PROTECTED
-			</h2>
-		</div>
-		</div>
-
-
-		<nav id="site-navigation" class="main-navigation site-header__navigation" role="navigation">
+		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'triumph' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
