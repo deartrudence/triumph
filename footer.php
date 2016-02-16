05930201605845983cd9fa9d875acd1cb3133a07
deartrudence <trudy.macnabb@gmail.com>
2016-02-16 12:04:29 -0500

    footer

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<p>Copyright &copy; <?php echo date('Y'); ?> Triumph Capital Limited. All Rights Reserved. Developed by <a href="#">Up At Five</a>
+			<p>Copyright &copy; <?php echo date('Y'); ?> Triumph Capital Limited. All Rights Reserved. Developed by <a href="http://www.upatfive.ca/" target="_blank">Up At Five</a>
 			<p>Triumph Capital Limited | 171 East Liberty Street Suite 272 Toronto, ON M6K 3P6 | (647) 345-0611 | 1-888-315-7781</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
